a54f56251938df3420a04b9060bd0f7eeaa28063
Thomas Ricketts <ricketta@miamioh.edu>
2014-11-07 20:21:44 -0500

    Added commenting

--- Assignment4/Test.cpp
+++ Assignment4/Test.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 using namespace ods;
 
-void test1() {
+void test1() { // Test 1 includes questions 1 & 2
 	BinarySearchTree<BSTNode1<int>, int> tree;
 	tree.add(15);
 	tree.add(14);
@@ -28,9 +28,19 @@ void test1() {
 	} else {
 		cout << "The tree is not balanced" << endl;
 	}
+
+	cout << "Removing Nodes..." << endl;
+	tree.remove(18);
+	tree.remove(17);
+
+	if (tree.isBalanced() == 1) {
+		cout << "The tree is balanced" << endl;
+	} else {
+		cout << "The tree is not balanced" << endl;
+	}
 }
 
-void test2() {
+void test2() { // Test 2 includes questions 3 & 4
 	BinarySearchTree<BSTNode1<int>, int> tree;
 	tree.add(15);
 	tree.add(14);
@@ -40,8 +50,8 @@ void test2() {
 	cout << "Testing getNode() for value 18: " << ((tree.getNode(18)->x))
 			<< endl;
 	DLList<int> list = tree.getLE(17);
-	cout << "Listing all values from 17 and below in the binary tree: " << endl;
-	for (int i = 0; i < list.size()-1; i++) {
+	cout << "Listing all values from 17 and below in the binary tree:" << endl;
+	for (int i = 0; i < list.size() - 1; i++) {
 		cout << list.get(i) << endl;
 	}
 }
