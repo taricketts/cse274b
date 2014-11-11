dd65f841c6c3dc572248d498e456605cc5d2202d
Thomas Ricketts <ricketta@miamioh.edu>
2014-11-11 11:40:07 -0500

    Empty husk of Assignment 5

---
+++ Assignment5/DataStructureTester.h
@@ -0,0 +1,59 @@
+/*
+ * DataStructureTester.h
+ *
+ *  Created on: Nov 8, 2014
+ *      Author: Tom
+ */
+
+#ifndef DATASTRUCTURETESTER_H_
+#define DATASTRUCTURETESTER_H_
+
+template<class T>
+class DataStructureTester<T>{
+
+public:
+	int DoSequentialAdd(int start, int end, int step);
+	int DoRandomAdd(int n);
+	int DoSequentialRemove(int start, int end, int step);
+	int DoRandomRemove(int n);
+	int DoSequentialFind(int start, int end, int step);
+	int DoRandomFind(int n);
+};
+
+template<class T>
+DataStructureTester<T>::DataStructureTester(){}
+
+template<class T>
+DataStructureTester<T>::~DataStructureTester(){}
+
+template<class T>
+int DataStructureTester<T>::DoSequentialAdd<T>(int start, int end, int step){
+	return 0;
+}
+
+template<class T>
+int DataStructureTester<T>::DoSequentialRemove<T>(int start, int end, int step){
+	return 0;
+}
+
+template<class T>
+int DataStructureTester<T>::DoSequentialFind<T>(int start, int end, int step){
+	return 0;
+}
+
+template<class T>
+int DataStructureTester<T>::DoRandomAdd<T>(int n){
+	return 0;
+}
+
+template<class T>
+int DataStructureTester<T>::DoRandomRemove<T>(int n){
+	return 0;
+}
+
+template<class T>
+int DataStructureTester<T>::DoRandomFind<T>(int n){
+	return 0;
+}
+
+#endif /* DATASTRUCTURETESTER_H_ */
